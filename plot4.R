5ae7f604288f58450bb3869388a77a2d04f70ab0
kavishant87 <69582100+kavishant87@users.noreply.github.com>
2020-11-27 17:32:09 +0530

    Update plot4.R

--- plot4.R
+++ plot4.R
@@ -11,10 +11,10 @@ base1<- subset(baseplot1,Date %in% c("1/2/2007","2/2/2007"))
 ##time format using strptime function.
 base1$Time <- strptime(paste(base1$Date,base1$Time, sep=" "), "%d/%m/%Y %H:%M:%S") 
 
+##Base Plot 4 and save as png file format.
 png("plot4.png", width=480, height=480)
 par(mfrow = c(2, 2), mar=c(4,4,2,1), oma=c(0,0,2,0)) 
 
-##Base Plot 4
 ##plot row (1,1) 
 
 with(base1, {
